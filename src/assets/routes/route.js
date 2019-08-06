cd949e43f6b92882fd2ad3b5defe4cd3a2391f70
COOMECIPAR\gustavo.penayo <penayo.gustavo@gmail.com>
2019-08-06 14:52:05 -0400

    Generación automática de api con fastify

--- src/assets/routes/route.js
+++ src/assets/routes/route.js
@@ -1,35 +1,50 @@
-
+var vueApiQuery = require('../middleware/vueApiQueryToSequelize')(models, <%= modelName %>)
 /**
  * Get All items with get method
  */
-fastify.get('<%= route %>', { beforeHandler: () => { next() } }, async (request, reply) => {
-  let <%= collectionName %> = await <%= modelName %>.findAll(request.query)
+fastify.get('<%= route %>', { beforeHandler: vueApiQuery }, async (request, reply) => {
+  let <%= collectionName %>
+  try { 
+    <%= collectionName %> = await <%= modelName %>.findAll(request.query)
+  } catch (error) {
+    console.log(error)
+    return reply.code(500).send(error)
+  }
   return <%= collectionName %>
 })
-
+<%= "\n" %>
 /**
  * Get one item with get method and :id params
  */
-fastify.get('<%= route + "/:id" %>', { beforeHandler: () => { next() } }, async (request, reply) => {
-  let <%= collectionName %> = await <%= modelName %>.findAll(request.query)
+fastify.get('<%= route + "/:id" %>', { beforeHandler: vueApiQuery }, async (request, reply) => {
+  let <%= collectionName %>
+  try {
+    <%= collectionName %> = await <%= modelName %>.findOne(request.query)
+  } catch (error) {
+    console.log(error)
+    return reply.code(500).send(error)
+  }
   return <%= collectionName %>
 })
-
+<%= "\n" %>
 /**
  * Add item sending and object with post method
  */
 fastify.post('<%= route %>', async (request, reply) => {
   var <%= instanceName %>
   try {
-    <%= instanceName %> = await <%= modelName %>.create(request.body)
-  } catch (err) {
-    reply.code(500).send({err: err});
-    return
+    if (Array.isArray(request.body)) {
+      <%= instanceName %> = await <%= modelName %>.bulkCreate(request.body, { returning: true })
+    } else {
+      <%= instanceName %> = await <%= modelName %>.create(request.body)
+    }
+  } catch (error) {
+    return reply.code(500).send(error)
   }
 
-  return reply.send({ success: true, message: '<%= modelName + " Registered successfully." %>', <%= instanceName %> })
+  return <%= instanceName %>
 })
-
+<%= "\n" %>
 /**
  * Modify and item sending and id with put method
  */
@@ -38,18 +53,16 @@ fastify.put('<%= route + "/:id" %>', async (request, reply) => {
     try {
       await <%= modelName %>.update(request.body, { where: { id: request.params.id }})
     } catch(err) {
-      return reply.code(500).send({ error: err })
+      return reply.code(500).send(error)
     }
 
-    return reply.send({
-      success: true,
-      message: 'Update Succefully',
-      <%= instanceName %>: await <%= modelName %>.findOne({ where: { id: request.params.id }})})
+    <%= instanceName %> = await <%= modelName %>.findOne({ where: { id: request.params.id }})
+    return <%= instanceName %>
   } else {
     return reply.code(500).send({ message: 'Must send an Id!' })
   }
 })
-
+<%= "\n" %>
 /**
  * Delete and item sending and id with delete method
  */
@@ -58,7 +71,7 @@ fastify.delete('<%= route + "/:id" %>', async (request, reply) => {
     try {
       await <%= modelName %>.destroy({ where: { id: request.params.id }})
     } catch(err) {
-      return reply.code(500).send({ error: err })
+      return reply.code(500).send(error)
     }
 
     return reply.send({ success: true, message: 'Delete Succefully'})
